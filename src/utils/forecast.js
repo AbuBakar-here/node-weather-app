e9b74326760d8131863c9446ce37e6fd47589f60
AbuBakar-here <abubakargone@gmail.com>
2022-04-13 20:43:34 +0500

    Added humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, lon, api, callback, unit = 'm') => {
         } else if (body.success === false) {
             callback('Unable to find this location. Please try another location.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. And humidity is ' + body.current.humidity + '%.')
         }
     })
 }
